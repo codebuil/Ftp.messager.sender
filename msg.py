87c2c890a1cea1e70f131248d94311f3fe4c496a
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-11-17 12:18:43 +0000

    Add files via upload

--- msg.py
+++ msg.py
@@ -1,27 +1,29 @@
 import datetime
 import os
 from ftplib import FTP
-
+import random
+counter=0
 def criar_mensagem():
     # Obtém a data e hora atual
     agora = datetime.datetime.now()
     data_hora = agora.strftime("%Y-%m-%d %H:%M:%S")
 
     # Solicita ao usuário uma mensagem
+    title= input("message title: ")
     mensagem = input("Digite a mensagem que deseja enviar: ")
-
+    
     # Formata a mensagem
     mensagem_formatada = f"{data_hora},{mensagem}"
 
-    return mensagem_formatada
+    return mensagem_formatada,title
 
 def escrever_log_local(mensagem):
     with open("data.tmp", "w") as arquivo:
         arquivo.write(mensagem + "\n")
 
-def enviar_para_telemovel(mensagem):
+def enviar_para_telemovel(mensagem,title):
     # Configurações FTP
-    endereco_ftp = "192.168.1.5"
+    endereco_ftp = "192.168.1.6"
     porta_ftp = 2121
 
     # Conecta ao servidor FTP
@@ -30,7 +32,8 @@ def enviar_para_telemovel(mensagem):
         ftp.login()
 
         # Nome do arquivo no telemóvel (data e hora sem espaços)
-        nome_arquivo = datetime.datetime.now().strftime("%Y%m%d_%H%M%S.txt")
+        rnd=random.randint(0,10000000000 )
+        nome_arquivo = title+f"{rnd}"
 
 
         # Escreve o arquivo temporário no telemóvel
@@ -41,7 +44,8 @@ def enviar_para_telemovel(mensagem):
 print("\x1bc\x1b[43;30mstart application:")
 
 # Chama a função para criar e enviar a mensagem imediatamente
-mensagem = criar_mensagem()
+title,mensagem = criar_mensagem()
 escrever_log_local(mensagem)
-enviar_para_telemovel(mensagem)
+enviar_para_telemovel(mensagem,title)
+
 
